ed2fae26362d87a6b830595face51510615f58ec
rajpatkar <rajan.patkar.email@gmail.com>
2023-02-16 16:56:24 -0800

    Edited sabertooth_code

--- control/arduino/controller_test/sabertooth_code.ino
+++ control/arduino/controller_test/sabertooth_code.ino
@@ -1,9 +1,23 @@
+// !!UNFINISHED!!
+
 #include <Sabertooth.h>
 
-SoftwareSerial mySerial1(19,18);
-SoftwareSerial mySerial2(17,16);
-Sabertooth ST[2] = { Sabertooth(128,mySerial1), Sabertooth(129,mySerial2) }; //128 ->Left and right Drive, 129->Bucket Ladder
-byte OdroidIn[2];
+/* available addresses for packetized serial mode are 128-135
+ * i.e. switches 1-3 are DOWN and the rest are up to user choice
+ *
+ * Description from http://www.dimensionengineering.com/datasheets/SabertoothDIPWizard/start.htm:
+ * Packetized serial mode will allow you to control many motors using just one shared serial wire
+ * Bytes are 8N1, and the data rate is automatically detected when you send 0xAA to the Sabertooth
+ * Switch 3 enables lithium cutoff mode
+ */
+
+// Abbreviations: st -> Sabertooth, dr -> drive, bl -> rbucket/ladder
+// serial input -> odroid
+Sabertooth st_dr(128, Serial1);
+byte odroid_dr;
+// Sabertooth st_bl(129, Serial1);
+// byte odroid_bl;
+byte odroid_in[2];
 
 #define dir_1 22
 #define pwm_1 2
@@ -15,122 +29,133 @@ byte OdroidIn[2];
 #define dir_4 25
 #define pwm_4 5
 
-
-
-
 void setup()
 {
-  SabertoothTXPinSerial.begin(9600);
-  Sabertooth::autobaud(SabertoothTXPinSerial);
-  mySerial1.begin(9600);
-  Serial.begin(9600); //serial input from Odroid.
-  
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  st_dr.autobaud();
 
-pinMode(pwm_1,OUTPUT);
-pinMode(dir_1,OUTPUT);
-pinMode(pwm_2,OUTPUT);
-pinMode(dir_2,OUTPUT);
+  pinMode(pwm_1, OUTPUT);
+  pinMode(dir_1, OUTPUT);
+  pinMode(pwm_2, OUTPUT);
+  pinMode(dir_2, OUTPUT);
 
-pinMode(pwm_3,OUTPUT);
-pinMode(dir_3,OUTPUT);
-pinMode(pwm_4,OUTPUT);
-pinMode(dir_4,OUTPUT);
+  pinMode(pwm_3, OUTPUT);
+  pinMode(dir_3, OUTPUT);
+  pinMode(pwm_4, OUTPUT);
+  pinMode(dir_4, OUTPUT);
 
+  st_dr.motor(1, 0);
+  st_dr.motor(2, 0);
+}
 
+void CRC_check()
+{
+
+  unsigned int odroid_in_concat = odroid_in[1] + odroid_in[0] * 256; // concatenates odroid_in into a 16-bit int
+  unsigned int CRC = 18;                                             // CRC error checking
+  unsigned int msg = odroid_in_concat % 2048;
+  unsigned int checksum = odroid_in_concat / 2048;
+  unsigned int curr = msg * 32 + checksum;
+
+  int a, index;
+  while (curr >= 32)
+  {
+    index = largestOneIndex(curr);
+    a = CRC << (index - 4);
+    curr ^= a;
+  }
+  if (curr)
+  {
+    // CRC came back bad
+    // do some error handling here??
+    return;
+  }
 }
 
 void loop()
 {
-   
-  Serial.readBytes(OdroidIn,2); 
-   unsigned int OdroidInInt = OdroidIn[1]+OdroidIn[0]*256;
-   unsigned int CRC = 18;
-   unsigned int msg = OdroidInInt % 2048;
-   unsigned int checksum = OdroidInInt / 2048;
-   unsigned int curr = msg*32 + checksum;
 
-   int a, index;
-   while (curr >= 32) {
-    index = largestOneIndex(curr);
-    a = CRC << (index - 4);
-    curr ^= a;
-   }
-   if (curr) {
-    // CRC came back bad
-    // do some error handling here??
-    return;
-   }
-   
-  //First byte: contains motorVal and checksum, in the Order Checksum:[7:3],motorVal[2:0]
-  //Second byte: is the power value.
+  Serial.readBytes(odroid_in, 2);
+  CRC_check();
+  // First byte: contains motorVal and checksum, in the Order Checksum:[7:3],motorVal[2:0]
+  // Second byte: is the power value.
   /*
    * Input[0],[1],[2] correspond to motor
-  KEY:
-  Motor ID: 
-0: Left Drive
-1: Right Drive
-2: Bucket Ladder M
-3: dump conveyor LA
-4: bucket ladder LA
-5: agitator
-6: dump conveyor M
-7: stops motors
-  */
-  int motorVal = OdroidIn[0] % 8; 
-  int power = OdroidIn[1] %128;
-  if(OdroidIn[1]/128 == 1 && motorVal<3){
-      power*=-1;
-    }
-    if(motorVal == 7){
-      Stop();
-      } 
-  else if(motorVal<3){ //call a sabertooth for the action
-    ST[(motorVal%4)/2].motor(motorVal%2+1,power);
-    }
-  else{//call a linAC
-    linAC(power,motorVal);
-    }
- 
+   * KEY:
+   * Motor ID:
+   * 0: Left Drive
+   * 1: Right Drive
+   * 2: Bucket Ladder M
+   * 3: dump conveyor LA
+   * 4: bucket ladder LA
+   * 5: agitator
+   * 6: dump conveyor M
+   * 7: stops motors
+   */
+  int motorVal = odroid_in[0] % 8;
+  int power = odroid_in[1] % 128;
+  if (odroid_in[1] / 128 == 1 && motorVal < 3)
+  {
+    power *= -1;
+  }
+  if (motorVal == 7)
+  {
+    Stop();
+  }
+  else if (motorVal < 3)
+  { // call a sabertooth for the action
+    ST[(motorVal % 4) / 2].motor(motorVal % 2 + 1, power);
+  }
+  else
+  { // call a linAC
+    linAC(power, motorVal);
+  }
 }
 
- void linAC(int power, int motor){
-    switch(motor) {
+void linAC(int power, int motor)
+{
+  switch (motor)
+  {
+  case 3:
+    digitalWrite(dir_1, (int)(odroid_in[1] / 128)); // controls the direction the motor;
+    analogWrite(pwm_1, power * 2);
+    break;
+  case 4:
+    digitalWrite(dir_2, (int)(odroid_in[1] / 128)); // controls the direction the motor;
+    analogWrite(pwm_2, power * 2);
+    break;
+  case 5:
+    digitalWrite(dir_3, (int)(odroid_in[1] / 128)); // controls the direction the motor;
+    analogWrite(pwm_3, power * 2);
+    break;
 
-   case 3 :
-      digitalWrite(dir_1, (int)(OdroidIn[1]/128)); //controls the direction the motor;
-      analogWrite(pwm_1, power*2); 
-      break;   case 4 :
-      digitalWrite(dir_2, (int)(OdroidIn[1]/128)); //controls the direction the motor;
-      analogWrite(pwm_2, power*2);
-      break; 
-  case 5 :
-      digitalWrite(dir_3, (int)(OdroidIn[1]/128)); //controls the direction the motor;
-      analogWrite(pwm_3, power*2);
-      break;
-  
-   case 6 :
-      digitalWrite(dir_4, (int)(OdroidIn[1]/128)); //controls the direction the motor;
-      analogWrite(pwm_4, power*2
-      break;  
-  
-  default : 
-   return;
+  case 6:
+    digitalWrite(dir_4, (int)(odroid_in[1] / 128)); // controls the direction the motor;
+    analogWrite(pwm_4, power * 2);
+    break;
+
+  default:
+    return;
+  }
 }
-  
+void Stop()
+{
+  for (int i = 3; i < 7; i++)
+  {
+    linAC(0, i);
   }
-void Stop(){
-  for(int i = 3; i<7; i++){
-    linAC(0,i);
-    }
-  ST[0].stop();
-  ST[1].stop();
-  
-  }
+  st_dr.stop();
+  // st_bl.stop();
+}
 
-int largestOneIndex(unsigned int curr) {
+int largestOneIndex(unsigned int curr)
+{
   int i = 15;
-  while (i >= 0) {
-    if (curr & (1 << i)) {
+  while (i >= 0)
+  {
+    if (curr & (1 << i))
+    {
       break;
     }
     i--;
