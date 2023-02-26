ccd081cdcd42798778bc00c707c6d3a03e4c64d1
ttchalakov <teodor.tch@gmail.com>
2023-02-26 10:59:33 -0600

    Added example world

--- navigation/launch/display.launch.py
+++ navigation/launch/display.launch.py
@@ -7,6 +7,7 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='navigation').find('navigation')
     default_model_path = os.path.join(pkg_share, 'src/description/robot_configuration.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    world_path=os.path.join(pkg_share, 'world/my_world.sdf'),
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -61,8 +62,8 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
-                                     output='screen'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path]
+                                      , output='screen'),
         joint_state_publisher_node,
         # joint_state_publisher_gui_node,
         robot_state_publisher_node,
