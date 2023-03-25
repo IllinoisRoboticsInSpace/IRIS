8265cd4648b3ab54f783c15bf031542395ab86ac
ttchalakov <teodor.tch@gmail.com>
2023-03-24 21:23:19 -0700

    Added arduino ide install logic

--- install_scripts/install_pc_deps.sh
+++ install_scripts/install_pc_deps.sh
@@ -9,3 +9,20 @@ echo "Installing Realsense SDK"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
 sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
+
+
+# Install Arduino 2 IDE
+cwd=$(pwd)
+cd ~/
+wget https://downloads.arduino.cc/arduino-ide/arduino-ide_2.0.4_Linux_64bit.AppImage -O ArduinoIDE2.AppImage
+sudo chmod +x ArduinoIDE2.AppImage
+
+# Install Sabertooth
+sudo apt-get install unzip
+mkdir -p ~/Arduino/libraries
+cd ~/Arduino/libraries
+wget https://www.dimensionengineering.com/software/SabertoothArduinoLibraries.zip
+unzip SabertoothArduinoLibraries.zip
+rm SabertoothArduinoLibraries.zip
+sudo usermod -a -G dialout iris # enable perimission requires restart
+cwd=$(pwd)
