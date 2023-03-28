f0e41c72ce4dcc00b75e9899d1545bbefbbabacb
ttchalakov <43731266+ttchalakov@users.noreply.github.com>
2023-03-28 18:52:47 -0500

    Update motor_pin_config.h

--- control/arduino/sabertooth_driver/motor_pin_config.h
+++ control/arduino/sabertooth_driver/motor_pin_config.h
@@ -27,28 +27,28 @@
 // NOTE: Hardware Serial TX0 interferes with the usb serial output
 
 // Sabertooth 1
-// Motors: Drive Left, Drive Right
+// Motors (1,2): Right Back Bucket Ladder Motor, Drive Right
 #define DRIVE_SABER_ADDRESS 128        // DIP switch address
 #define DRIVE_SABER_SERIAL Serial1     // Hardware Serial TX1
 #define DRIVE_LEFT_MOTOR 1             // S1
 #define DRIVE_RIGHT_MOTOR 2            // S2
 
 // Sabertooth 2
-// Motors: A, B
+// Motors (1,2): Excavator Internal Motor, Excavator Threaded Rod Actuator
 #define S2_SABER_ADDRESS 129           // DIP switch address
 #define S2_SABER_SERIAL Serial2        // Hardware Serial TX2
 #define S2_LEFT_MOTOR 1                // S1
 #define S2_RIGHT_MOTOR 2               // S2
 
 // Sabertooth 3
-// Motors: A, B
+// Motors (1,2): Left Collection Motor, Left Drive Motor
 #define S3_SABER_ADDRESS 130            // DIP switch address
 #define S3_SABER_SERIAL Serial3         // Hardware Serial TX3
 #define S3_LEFT_MOTOR 1                 // S1
 #define S3_RIGHT_MOTOR 2                // S2
 
 // 10 Amp Board
-// Motors: A
+// Motors: Excavator Pivoting Linear Actuator
 #define A_DIRECTION 22
 #define A_PWM 2
 
