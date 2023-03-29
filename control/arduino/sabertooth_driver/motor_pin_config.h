41ec970a349a1b9f6f6cb04a57bbe9452c2eec6c
iris-uiuc <iris.uiuc@gmail.com>
2023-03-29 14:39:00 -0500

    Added button configs

--- control/arduino/sabertooth_driver/motor_pin_config.h
+++ control/arduino/sabertooth_driver/motor_pin_config.h
@@ -30,22 +30,22 @@
 // Motors S(1,2): Right Back Collection Motor, Drive Right
 #define S1_SABER_ADDRESS 128            // DIP switch address
 #define S1_SABER_SERIAL Serial1         // Hardware Serial TX1
-#define RIGHT_COLLECTION_MOTOR 1        // S1
-#define DRIVE_RIGHT_MOTOR 2             // S2
+#define RIGHT_COLLECTION_MOTOR 1       // M1
+#define DRIVE_RIGHT_MOTOR 2             // M2
 
 // Sabertooth 2
 // Motors S(1,2): Excavator Internal Motor, Excavator Threaded Rod Actuator
 #define S2_SABER_ADDRESS 129            // DIP switch address
 #define S2_SABER_SERIAL Serial2         // Hardware Serial TX2
-#define EXCAVATOR_INTERNAL_MOTOR 1      // S1
-#define EXCAVATOR_THREADED_ROD 2        // S2
+#define EXCAVATOR_INTERNAL_MOTOR 1      // M1
+#define EXCAVATOR_THREADED_ROD 2        // M2
 
 // Sabertooth 3
 // Motors S(1,2): Left Back Collection Motor, Left Drive Motor
 #define S3_SABER_ADDRESS 130            // DIP switch address
 #define S3_SABER_SERIAL Serial3         // Hardware Serial TX3
-#define LEFT_COLLECTION_MOTOR 1         // S1
-#define DRIVE_LEFT_MOTOR 2              // S2
+#define LEFT_COLLECTION_MOTOR 1         // M1
+#define DRIVE_LEFT_MOTOR 2              // M2
 
 // 10 Amp Board
 // Motors: Excavator Pivoting Linear Actuator
--- jetson/jetson/twist_serial.py
+++ jetson/jetson/twist_serial.py
@@ -3,6 +3,8 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import Vector3, Twist
 
+from sensor_msgs.msg import Joy
+
 from std_msgs.msg import String
 
 import serial
@@ -14,6 +16,10 @@ class twist_serial(Node):
 
         self.subscription = self.create_subscription(
             Twist, '/rover/cmd_vel', self.twist_callback, 10) #subscribes to cmd_vel topic
+
+        self.subscription = self.create_subscription(
+            Joy, '/joy', self.twist_callback, 10) #subscribes to joy topic
+        
         self.get_logger().info(f"Created node {self.get_name()}")
 
         self.publisher_ = self.create_publisher(String, 'controller', 10) #publishes to 'controller' topic
@@ -21,8 +27,17 @@ class twist_serial(Node):
         # USB Cable should be connected to programming port from the jetson
         self.arduino = serial.Serial(port = '/dev/ttyACM0',baudrate = 9600, timeout = .1) #initializes arduino serial port. make sure baudrate is same for arduino and python code
 
-        self.numMotor = [0,1] # Left Driver, Right Drive
-        self.power = [0,0]
+        # Motor ID:
+        # * 0: Left Drive
+        # * 1: Right Drive
+        # * 2: Left Back Collection Motor
+        # * 3: Right Back Collection Motor
+        # * 4: Excavator Internal Motor
+        # * 5: Excavator Threaded Rod Actuator
+        # * 6: Excavator Pivoting Linear Actuator
+        # * 7: stops motors
+        self.numMotor = [0,1,2,3,4,5,6]
+        self.power = [0,0,0,0,0,0,0]
 
     ####################################################################################################
 
@@ -70,12 +85,32 @@ class twist_serial(Node):
         returnMsg.data = 'power = ' + str(self.power[i]) + ' bytes: ' + str(self.power_to_bytes(i))
         self.publisher_.publish(returnMsg)
 
-    def twist_callback(self, msg: Twist):
-        x = max(min(msg.linear.x, 1.0), -1.0)
-        z = max(min(msg.angular.z, 1.0), -1.0)
+    def twist_callback(self, twist_msg: Twist, joy_msg: Joy):
+        x = max(min(twist_msg.linear.x, 1.0), -1.0)
+        z = max(min(twist_msg.angular.z, 1.0), -1.0)
         scale = 80
         self.power[0] = -int((x - z) * scale) 
         self.power[1] = int((x + z) * scale)
+        #added for joystick buttons
+        # for button_idx in range(len(joy_msg.buttons)):
+        #     self.power[button_idx+2] = 1 #offset to keet power for twist?; placeholder
+        if joy_msg.axes[0]=0.0 and round(joy_msg.axes[1])=1.0:
+            self.power[0] = scale
+        if joy_msg.axes[3]=0.0 and round(joy_msg.axes[4])=1.0:
+            self.power[1] = scale
+        if joy_msg.buttons[4]=1:
+            self.power[2] = scale
+        if joy_msg.buttons[5]=1:
+            self.power[3] = scale
+        if joy_msg.buttons[0]=1:
+            self.power[4] = scale
+        if joy_msg.buttons[1]=1:
+            self.power[5] = scale
+        if joy_msg.buttons[2]=1:
+            self.power[6] = scale
+        # if joy_msg.buttons[8]=1: #power
+
+
         self.limitPower()
         self.display_power(0)
         #self.get_logger().info(f"power:{self.power}")
