dffa266d574a19a2ad51bd4770d2ae67e8a66b61
iris-uiuc <iris.uiuc@gmail.com>
2023-03-30 18:50:46 -0500

    updates joystick button mappings and node

--- jetson/jetson/twist_serial.py
+++ jetson/jetson/twist_serial.py
@@ -14,11 +14,11 @@ class twist_serial(Node):
     def __init__(self):
         super().__init__('twist_serial')
 
-        self.subscription = self.create_subscription(
-            Twist, '/rover/cmd_vel', self.twist_callback, 10) #subscribes to cmd_vel topic
+        # self.subscription = self.create_subscription(
+        #     Twist, '/rover/cmd_vel', self.twist_callback, 10) #subscribes to cmd_vel topic
 
         self.subscription = self.create_subscription(
-            Joy, '/joy', self.twist_callback, 10) #subscribes to joy topic
+            Joy, '/joy', self.joy_callback, 10) #subscribes to joy topic
         
         self.get_logger().info(f"Created node {self.get_name()}")
 
@@ -38,6 +38,16 @@ class twist_serial(Node):
         # * 7: stops motors
         self.numMotor = [0,1,2,3,4,5,6]
         self.power = [0,0,0,0,0,0,0]
+
+        #motor states
+        self.LEFT_DRIVE = 0
+        self.RIGHT_DRIVE = 0
+        self.LEFT_BACK_COLL = 0
+        self.RIGHT_BACK_COLL = 0
+        self.EXC_INTERNAL = 0
+        self.EXC_THREAD_ROD = 0
+        self.EXC_PIVOT_LIN = 0
+
 
     ####################################################################################################
 
@@ -69,9 +79,9 @@ class twist_serial(Node):
             curr ^= a
         y = x + curr*256*8
         return y.to_bytes((y.bit_length() + 7) // 8, 'big')
-    def sendPower(self):
-        for i in range(len(self.power)):
-            self.arduino.write(self.power_to_bytes(i)) #writes power to serial0
+    # def sendPower(self):
+    #     for i in range(len(self.power)):
+    #         self.arduino.write(self.power_to_bytes(i)) #writes power to serial0
     
     ####################################################################################################
     def limitPower(self):
@@ -85,36 +95,92 @@ class twist_serial(Node):
         returnMsg.data = 'power = ' + str(self.power[i]) + ' bytes: ' + str(self.power_to_bytes(i))
         self.publisher_.publish(returnMsg)
 
-    def twist_callback(self, twist_msg: Twist, joy_msg: Joy):
-        x = max(min(twist_msg.linear.x, 1.0), -1.0)
-        z = max(min(twist_msg.angular.z, 1.0), -1.0)
-        scale = 80
-        self.power[0] = -int((x - z) * scale) 
-        self.power[1] = int((x + z) * scale)
-        #added for joystick buttons
-        # for button_idx in range(len(joy_msg.buttons)):
-        #     self.power[button_idx+2] = 1 #offset to keet power for twist?; placeholder
-        if joy_msg.axes[0]=0.0 and round(joy_msg.axes[1])=1.0:
-            self.power[0] = scale
-        if joy_msg.axes[3]=0.0 and round(joy_msg.axes[4])=1.0:
-            self.power[1] = scale
-        if joy_msg.buttons[4]=1:
-            self.power[2] = scale
-        if joy_msg.buttons[5]=1:
-            self.power[3] = scale
-        if joy_msg.buttons[0]=1:
-            self.power[4] = scale
-        if joy_msg.buttons[1]=1:
-            self.power[5] = scale
-        if joy_msg.buttons[2]=1:
-            self.power[6] = scale
-        # if joy_msg.buttons[8]=1: #power
+    # def twist_callback(self, twist_msg: Twist):
+    #     x = max(min(twist_msg.linear.x, 1.0), -1.0)
+    #     z = max(min(twist_msg.angular.z, 1.0), -1.0)
+    #     scale = 80
+    #     self.power[0] = -int((x - z) * scale) 
+    #     self.power[1] = int((x + z) * scale)
 
+    def joy_callback(self, joy_msg: Joy):
+        scale = 80      #power scale
+        axes_error = 0.5
 
-        self.limitPower()
-        self.display_power(0)
+        #Left Drive
+        curr_left_drive_state = (abs(joy_msg.axes[1])>axes_error)
+        if curr_left_drive_state != self.LEFT_DRIVE:
+            self.LEFT_DRIVE = curr_left_drive_state
+            self.power[0] = scale * joy_msg.axes[1] * self.LEFT_DRIVE
+
+            self.limitPower()
+            self.display_power(0)
+            self.arduino.write(self.power_to_bytes(0))
+        
+        #Right Drive
+        curr_right_drive_state = (abs(joy_msg.axes[4])>axes_error)
+        if curr_right_drive_state != self.RIGHT_DRIVE:
+            self.RIGHT_DRIVE = curr_right_drive_state
+            self.power[1] = scale * joy_msg.axes[4] * self.RIGHT_DRIVE
+
+            self.limitPower()
+            self.display_power(1)
+            self.arduino.write(self.power_to_bytes(1))
+        
+        #Left Back Collection
+        if joy_msg.buttons[4] != self.LEFT_BACK_COLL:
+            self.LEFT_BACK_COLL = joy_msg.buttons[4]
+            self.power[2] = scale * self.LEFT_BACK_COLL
+
+            self.limitPower()
+            self.display_power(2)
+            self.arduino.write(self.power_to_bytes(2))
+        
+        #Right Back Collection
+        if joy_msg.buttons[5] != self.RIGHT_BACK_COLL:
+            self.RIGHT_BACK_COLL = joy_msg.buttons[5]
+            self.power[3] = scale * self.RIGHT_BACK_COLL
+
+            self.limitPower()
+            self.display_power(3)
+            self.arduino.write(self.power_to_bytes(3))
+        
+        #Excavator Internal Motor
+        if joy_msg.buttons[0] != self.EXC_INTERNAL:
+            self.EXC_INTERNAL = joy_msg.buttons[0]
+            self.power[4] = scale * self.EXC_INTERNAL
+
+            self.limitPower()
+            self.display_power(4)
+            self.arduino.write(self.power_to_bytes(4))
+        
+        #Excavator Threaded Rod Actuator
+        if joy_msg.buttons[1] != self.EXC_THREAD_ROD:
+            self.EXC_THREAD_ROD = joy_msg.buttons[1]
+            self.power[5] = scale * self.EXC_THREAD_ROD
+
+            self.limitPower()
+            self.display_power(5)
+            self.arduino.write(self.power_to_bytes(5))
+        
+        #Excavator Pivoting Linear Actuator
+        if joy_msg.buttons[2] != self.EXC_PIVOT_LIN:
+            self.EXC_PIVOT_LIN = joy_msg.buttons[2]
+            self.power[6] = scale * self.EXC_PIVOT_LIN
+
+            self.limitPower()
+            self.display_power(6)
+            self.arduino.write(self.power_to_bytes(6))
+        
+        #Power Off
+        if joy_msg.buttons[8]==1:
+            for i in range(len(self.power)):
+                self.power[i] = 0
+
+                self.display_power(i)
+                self.arduino.write(self.power_to_bytes(i))
+        
         #self.get_logger().info(f"power:{self.power}")
-        self.sendPower()
+        # self.sendPower()
         
 
 def main(args=None):
