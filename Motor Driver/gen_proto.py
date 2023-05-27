7ab09a30a713e0eb6b2090b5a01891d8926f017b
ttchalakov <teodor.tch@gmail.com>
2023-05-27 15:07:36 -0700

    Began build system for protobufs

---
+++ Motor Driver/gen_proto.py
@@ -0,0 +1,17 @@
+import os
+
+Import("env")
+
+# env.Execute("echo \"asdf\"")
+print("Generating embedded protobuf files")
+embedded_proto_path = "lib/EmbeddedProto"
+lib_base_path = "lib/IRISMotorDriver"
+env.Execute(f"protoc --plugin=protoc-gen-eams={embedded_proto_path}/protoc-gen-eams" + " "
+            + f"-I{lib_base_path}/proto" + " "
+            + f"--eams_out={lib_base_path}/src/generated" + " "
+            + f"{lib_base_path}/proto/commands.proto")
+
+# env.BuildSources(
+#     os.path.join("$BUILD_DIR", "external", "build"),
+#     os.path.join("$PROJECT_DIR", "external", "sources")
+# )
