3cd7cf4057e96779cbc4e0348ceb430d64a2a3b8
ttchalakov <teodor.tch@gmail.com>
2023-05-30 09:37:04 -0700

    Fixed macro function name dep conflict

--- MotorDriver/src/main.cpp
+++ MotorDriver/src/main.cpp
@@ -1,6 +1,11 @@
-#include <Arduino.h>
+#include "generated/commands.h"
 #include "Sabertooth.h"
 #include "MotorDriver.h"
+
+// Must be included last so no macro and function name definition conflict
+// happens between dependencies. This fix needs to be applied to all dependencies.
+// To learn more read this: https://community.platformio.org/t/nrf52832-include-set-breaks-compilation-on-platformio-but-compiles-fine-on-arduinoide/12237/2
+#include <Arduino.h>
 
 Sabertooth ST(128);
 
