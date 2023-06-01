01552c6b681c1ea3900cf5c5debe2cf66de65a18
ttchalakov <teodor.tch@gmail.com>
2023-06-01 13:17:45 -0700

    Added repeated proto messages support

--- MotorDriver/gen_proto.py
+++ MotorDriver/gen_proto.py
@@ -9,5 +9,6 @@ embedded_proto_path = "lib/EmbeddedProto"
 lib_base_path = "lib/IRISMotorDriver"
 env.Execute(f"protoc --plugin=protoc-gen-eams={embedded_proto_path}/protoc-gen-eams" + " "
             + f"-I{lib_base_path}/proto" + " "
+            + f"-I{embedded_proto_path}/generator" + " "
             + f"--eams_out={lib_base_path}/include/generated" + " "
             + f"{lib_base_path}/proto/*.proto")
