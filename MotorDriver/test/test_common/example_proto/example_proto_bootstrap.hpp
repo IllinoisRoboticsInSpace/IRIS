b238b6f0b79d4d6276cff54d09135262c665a5dd
ttchalakov <teodor.tch@gmail.com>
2023-06-06 12:03:41 -0700

    Added example nested proto message tests

--- MotorDriver/test/test_common/example_proto/example_proto_bootstrap.hpp
+++ MotorDriver/test/test_common/example_proto/example_proto_bootstrap.hpp
@@ -3,4 +3,5 @@
 void RUN_EXAMPLE_PROTO_TESTS(void)
 {
     RUN_TEST(basic_types_test);
+    RUN_TEST(nested_message_test);
 }
--- MotorDriver/test/test_common/example_proto/proto_types_test.hpp
+++ MotorDriver/test/test_common/example_proto/proto_types_test.hpp
@@ -73,3 +73,43 @@ void basic_types_test(void)
     TEST_ASSERT_EQUAL_FLOAT(1.5, deserialized_message.get_float_t());
     TEST_ASSERT_EQUAL_DOUBLE(-10.5, deserialized_message.get_double_t());
 }
+
+void nested_message_test(void)
+{
+    reset();
+
+    Nested message;
+    A nested_message;
+    nested_message.set_a(1);
+    message.set_nested_a(nested_message);
+    TEST_ASSERT_EQUAL_INT32(1, message.get_nested_a().get_a());
+    // Accessing the nested message and editing it.
+    message.mutable_nested_a().set_a(2);
+    TEST_ASSERT_EQUAL_INT32(2, message.get_nested_a().get_a());
+
+    uint32_t size = message.serialized_size();
+    TEST_ASSERT_GREATER_OR_EQUAL_UINT32(0, size);
+
+    // Serialize Message
+    auto serialization_status = message.serialize(write_fixed_buffer);
+    if(::EmbeddedProto::Error::NO_ERRORS != serialization_status)
+    {
+        TEST_FAIL_MESSAGE("Serialization Produced Error");
+    }
+    uint8_t* write_data_ptr = write_fixed_buffer.get_data();
+    uint32_t write_data_size = write_fixed_buffer.get_size();
+    TEST_ASSERT_EQUAL_INT32(size, write_data_size);
+
+    // Deserialize Message
+    uint8_t* read_data_ptr = read_fixed_buffer.get_data();
+    memcpy(read_data_ptr, write_data_ptr, write_data_size);
+    read_fixed_buffer.set_bytes_written(write_data_size);
+
+    Nested deserialized_message;
+    auto deserialize_status = deserialized_message.deserialize(read_fixed_buffer);
+    if(::EmbeddedProto::Error::NO_ERRORS != deserialize_status)
+    {
+        TEST_FAIL_MESSAGE("Deserialization Produced Error");
+    }
+    TEST_ASSERT_EQUAL_INT32(2, deserialized_message.get_nested_a().get_a());
+}
