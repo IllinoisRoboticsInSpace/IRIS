bab4607b9ca99164cdd6e803564d01a8a1e98cb5
ttchalakov <teodor.tch@gmail.com>
2023-06-18 21:03:40 -0500

    Added part 3 ubuntu 20 upgrade

--- install_scripts/ubuntu_20_upgrade_part_2.sh
+++ install_scripts/ubuntu_20_upgrade_part_2.sh
@@ -4,8 +4,9 @@ printf "Select all default options for upgrade\n"
 sudo do-release-upgrade
 
 # Uncomment lines with tokens
-sed -i '/WaylandEnable/s/^#//g' /etc/gdm3/custom.conf
-sed -i '/"nvidia"/s/^#//g' /etc/X11/xorg.conf
+# https://stackoverflow.com/a/27355109
+sudo sed -i '/WaylandEnable/s/^#//g' /etc/gdm3/custom.conf
+sudo sed -i '/"nvidia"/s/^#//g' /etc/X11/xorg.conf
 
 sudo sed -i -e '/Prompt=/ s/=.*/=never/' /etc/update-manager/release-upgrades
 
---
+++ install_scripts/ubuntu_20_upgrade_part_3.sh
@@ -0,0 +1,31 @@
+sudo rm -rf /usr/share/vulkan/icd.d
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+
+sudo rm /usr/share/applications/vpi1_demos
+
+cwd=$(pwd)
+cd /usr/share/nvpmodel_indicator
+sudo mv nv_logo.svg no_logo.svg
+cd $cwd
+
+source_folder="/etc/apt/sources.list.d"
+sudo sed -i '/cuda-repo/s/^#//g' $source_folder/cuda-l4t*.list
+sudo sed -i '/jetson/s/^#//g' $source_folder/nvidia-l4t*.list
+sudo sed -i '/visionworks/s/^#//g' $source_folder/visionworks-*.list
+
+# Add gcc and g++ alternatives
+sudo apt-get install gcc-8 g++-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
+
+# if you want to make a selection use these commands
+# sudo update-alternatives --config gcc
+# sudo update-alternatives --config g++
+
+# Disable the Power Saving Blank Screen option in the settings as it gives unstable results when rebooting from sleep mode.
+# It is found in settings under "Power".
