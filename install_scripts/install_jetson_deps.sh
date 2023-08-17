14c1202a867461870e8f75846023c8bf0c6d0203
ttchalakov <teodor.tch@gmail.com>
2023-08-17 15:56:39 -0500

    Made ROS_DOMAIN optional setting

--- install_scripts/install_jetson_deps.sh
+++ install_scripts/install_jetson_deps.sh
@@ -11,6 +11,9 @@ echo "export CUDA_HOME=/usr/local/cuda" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64" >> ~/.bashrc
 echo "export PATH=$PATH:$CUDA_HOME/bin" >> ~/.bashrc
 
+# Setup local network ROS communication
+echo "export ROS_DOMAIN_ID=1" >> ~/.bashrc
+
 export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64
 export PATH=$PATH:$CUDA_HOME/bin
