00a8528a0a8ce73a00367a14111312b1ba450c34
Willow <crosbywilliam@gmail.com>
2023-11-04 11:29:16 -0700

    uppading main.cpp with  encoder code

--- MotorDriver/src/main.cpp
+++ MotorDriver/src/main.cpp
@@ -1,23 +1,55 @@
 #include "Sabertooth.h"
 #include "MotorDriver.h"
-
+#include "RotaryEncoder.h"
+#include "PID_v1.h"
 // Must be included last so no macro and function name definition conflict
 // happens between dependencies. This fix needs to be applied to all dependencies.
 // To learn more read this: https://community.platformio.org/t/nrf52832-include-set-breaks-compilation-on-platformio-but-compiles-fine-on-arduinoide/12237/2
 #include <Arduino.h>
 
-Sabertooth ST(128);
+#define TICKS_TO_DEG 0.3515625 // 360/1024 
+//#define SABERTOOTH_PIN A5
+#define ENCODER_PIN_IN A0
+#define ENCODER_PIN_OUT A1
 
-MotorDriver driver;
+double encoder_read = 0;
+double prev_encoder_read = 0;
+// double temp = 0;
+double motor_write = 0;
+double setpoint = 360;
+double Kp = 1; double Ki = 0.1; double Kd = 0.01;
+Sabertooth sabertooth(128);
+RotaryEncoder encoder(ENCODER_PIN_IN, ENCODER_PIN_OUT, RotaryEncoder::LatchMode::FOUR3);
+PID pid(&encoder_read, &motor_write, &setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
-  SabertoothTXPinSerial.begin(9600); // 9600 is the default baud rate for Sabertooth packet serial.
-  Serial.begin(112500);//For printing
-  ST.autobaud();
+  pid.SetOutputLimits(-127,127);
+  
+  SabertoothTXPinSerial.begin(9600);
+  sabertooth.autobaud();
 
-  driver.init_motor_driver();
+  Serial.begin(112500);
+  Serial.println("Encoder and PID Test");
+
+  sabertooth.motor(0);
+  //sabertooth.motor(15);
+
 }
 
 void loop() {
-  driver.update();
+  encoder.tick();
+  prev_encoder_read = encoder_read;
+  //sabertooth.motor(10);
+  //encoder.setPosition(encoder.getPosition() + 1);
+  
+  encoder_read = encoder.getPosition(); //* TICKS_TO_DEG;
+  pid.Compute();
+  //sabertooth.motor((int)motor_write);
+  if(prev_encoder_read != encoder_read){
+    Serial.println(encoder_read);
+  }
+  if(encoder_read > 100 || encoder_read < -100){
+    sabertooth.motor(0);
+  }
+  //encoder.tick();
 }
