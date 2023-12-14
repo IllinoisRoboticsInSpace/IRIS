c6c9377806124fde10dd96a9b539f4cb1e29ccd8
ttchalakov <teodor.tch@gmail.com>
2023-12-14 17:19:09 -0600

    Added faster regular print function

--- MotorDriver/lib/IRISMotorDriver/include/DebugTools.h
+++ MotorDriver/lib/IRISMotorDriver/include/DebugTools.h
@@ -3,6 +3,8 @@
 
 #include <Arduino.h>
 
+// Must start usb serial transfer by running:
+// Serial.begin(DEFAULT_HOST_SERIAL_BAUD_RATE);
 // Do not commit with ENABLE_DEBUG set to true
 #define ENABLE_DEBUG false
 
@@ -13,13 +15,23 @@
         extern char debug_buffer[DEBUG_BUFFER_SIZE];
     #endif
 
-    #define DEBUG_PRINT(string, ...)              \
-    {                                             \
-    sprintf(debug_buffer, string, ##__VA_ARGS__); \
-    Serial.println(debug_buffer);                 \
+    #define DEBUG_PRINTF(string, ...)               \
+    {                                               \
+    sprintf(debug_buffer, string, ##__VA_ARGS__);   \
+    Serial.println(debug_buffer);                   \
     }
+
+    #define DEBUG_PRINT(string)                     \
+    {                                               \
+    Serial.println(string);                         \
+    }
+
 #else
-    #define DEBUG_PRINT(string, ...) {}
+    #define DEBUG_PRINTF(string, ...) {}
+    #define DEBUG_PRINT(string) {}
 #endif
+
+// Resource on counting number of input arguments in variadic macro:
+// https://stackoverflow.com/questions/2124339/c-preprocessor-va-args-number-of-arguments
 
 #endif
--- MotorDriver/lib/IRISMotorDriver/src/DebugTools.cpp
+++ MotorDriver/lib/IRISMotorDriver/src/DebugTools.cpp
@@ -3,5 +3,4 @@
 
 #if ENABLE_DEBUG == true
 char debug_buffer[DEBUG_BUFFER_SIZE];
-Serial.begin(DEFAULT_HOST_SERIAL_BAUD_RATE);
 #endif
