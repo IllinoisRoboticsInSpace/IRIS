bee015281e4414ced5c1ae3914205372891b62e5
Caleb Beversdorf <cbeve2@illinois.edu>
2023-12-22 10:59:18 -0800

    Progress on motor_driver

--- motor_driver/motor_driver/motor_driver.py
+++ motor_driver/motor_driver/motor_driver.py
@@ -1,17 +1,16 @@
 import serial
 from motor_driver.generated import commands_pb2
-SERIAL_BUFFER_BYTES = 64
-
-#Make new proto message for sending motor config and turnmotor message
-
+SERIAL_BUFFER_BYTES = 64 # What is this for
+MIN_MOTOR_ID = 0
+MAX_MOTOR_ID = 3
 
 class MotorConfig: 
 
     def __init__(self, motorID: int, serialPin: int, motorNum: int, address: int, inverted: bool = False):
-        if motorID < 1 or motorID > 4:
-            raise ValueError("motorID must be between 1 and 4")
+        if motorID < MIN_MOTOR_ID or motorID > MAX_MOTOR_ID:
+            raise ValueError("Invalid Motor ID")
         if motorNum < 1 or motorNum > 2:
-            raise ValueError("motorID must be between 1 and 2")
+            raise ValueError("motorNum must be between 1 and 2")
         self.motorID = motorID
         self.serialPin = serialPin
         self.motorNum = motorNum
@@ -25,36 +24,44 @@ class MotorDriver:
         self.debugState = False
         self.serialLine = serial.Serial(port = port, baudrate = baudrate, timeout = timeout)
         # List of Motor configs
-        self.motorConfigs = []
-        # Where do we create the motor config objects
-        # Where do we call the sendMotorConfig function
+        self.motorConfigs = [None] * MAX_MOTOR_ID
 
-
-
-
+    def initMotorDriver(self):
+        pass # Send all configurations?
         
-        
+    def resetDevice(self):
+        pass # Send Arduino a reset command?
+        # Reset Arduino to original state
 
     def setDebugMode(self, toggle: bool):
-        #Do last due to threading
+        #Do last due to threading NOT processing
+        #While loop reading same serial line
+        #Global variable to shut it down
+        # Test in different area
         pass
 
-    def sendMotorConfig(self, config: MotorConfig):
+    def setMotorConfig(self, config: MotorConfig):
+        self.motorConfigs[config.motorID] = config
+
+    def sendMotorConfig(self, motorID: int):
+        if motorID < MIN_MOTOR_ID or motorID > MAX_MOTOR_ID:
+            raise ValueError("Invalid Motor ID")
+
         message = commands_pb2.Serial_Message()
         message.opcode = commands_pb2.CONFIG_MOTOR
-        message.configData.motorID = config.motorID
+        message.configData.motorID = self.motorConfigs[motorID].motorID
         self.serialLine.write(message.SerializeToString())
         
-        message.configData.serialPin = config.serialPin
+        message.configData.serialPin = self.motorConfigs[motorID].serialPin
         self.serialLine.write(message.SerializeToString())
 
-        message.configData.motorNum = config.motorNum
+        message.configData.motorNum = self.motorConfigs[motorID].motorNum
         self.serialLine.write(message.SerializeToString())
 
-        message.configData.address = config.address
+        message.configData.address = self.motorConfigs[motorID].address
         self.serialLine.write(message.SerializeToString())
 
-        message.configData.inverted = config.inverted
+        message.configData.inverted = self.motorConfigs[motorID].inverted
         self.serialLine.write(message.SerializeToString())
 
     def stopMotors(self):
@@ -62,21 +69,15 @@ class MotorDriver:
         message.opcode = commands_pb2.STOP_ALL_MOTORS
         self.serialLine.write(message.SerializeToString())
 
-    def turnMotor(self, motorID: int, output: int):
-        if (output < -128 or output > 128):
-            raise ValueError("output must be between -128 and 128")
+    def turnMotor(self, motorID: int, output: float):
+        if (output < -1.0 or output > 1.0):
+            raise ValueError("output must be between -1.0 and 1.0")
+        if motorID < MIN_MOTOR_ID or motorID > MAX_MOTOR_ID:
+            raise ValueError("Invalid Motor ID")
 
         message = commands_pb2.Serial_Message()
-        if motorID == 1:
-            message.opcode = commands_pb2.TURN_MOTOR_1
-        elif motorID == 2:
-            message.opcode = commands_pb2.TURN_MOTOR_2
-        elif motorID == 3:
-            message.opcode = commands_pb2.TURN_MOTOR_3
-        elif motorID == 4:
-            message.opcode = commands_pb2.TURN_MOTOR_4
-        else:
-            raise ValueError("motorID must be between 1 and 4")
+        message.opcode = commands_pb2.TURN_MOTOR
         message.motorCommand.percentSpeed = output
+        message.motorCommand.motorID = motorID
 
         self.serialLine.write(message.SerializeToString())
