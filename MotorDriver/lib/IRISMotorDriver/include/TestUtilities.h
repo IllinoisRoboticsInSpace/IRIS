40d1250fa8d5621f7e3de70d6a1664d6d038d976
ttchalakov <teodor.tch@gmail.com>
2023-12-25 18:24:57 -0600

    Started test exec time macro

---
+++ MotorDriver/lib/IRISMotorDriver/include/TestUtilities.h
@@ -0,0 +1,16 @@
+#ifndef _TEST_UTILITIES_
+#define _TEST_UTILITIES_
+
+#include "unity.h"
+
+//Compile error: identifier "CLOCK_MONOTONIC" is undefined
+
+// #define RUN_TEST_TIMED(func)    \
+// {                               \
+//     UNITY_EXEC_TIME_START();    \
+//     RUN_TEST(func);             \
+//     UNITY_EXEC_TIME_STOP();     \
+//     UNITY_PRINT_EXEC_TIME();    \
+// }
+
+#endif
