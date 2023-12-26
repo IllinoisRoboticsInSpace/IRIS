e12e77f8e7daafbf05441b0350775b001221c4c3
ttchalakov <teodor.tch@gmail.com>
2023-12-25 23:24:48 -0600

    Cleaned up main.cpp

--- MotorDriver/src/main.cpp
+++ MotorDriver/src/main.cpp
@@ -1,5 +1,7 @@
 #include "Sabertooth.h"
 #include "MotorDriver.h"
+#include "DebugTools.h"
+#include "generated/commands.h"
 
 // Must be included last so no macro and function name definition conflict
 // happens between dependencies. This fix needs to be applied to all dependencies.
@@ -7,24 +9,20 @@
 #include <Arduino.h>
 
 #define DEFAULT_SABERTOOTH_BAUD_RATE 9600 // 9600 is the default baud rate for Sabertooth packet serial.
-#define DEFAULT_SABERTOOTH_PORT 128 // 128 is the default port (for some reason?)
 
-Sabertooth ST(DEFAULT_SABERTOOTH_PORT);
 MotorDriver driver;
 
 // Serial: line from us to arduino
 // Serial1: line from arduino to sabertooth
 
 void setup() {
-  //? below block in the init_motor_driver_function? or the constructor itself in MotorDriver
-  SabertoothTXPinSerial.begin(DEFAULT_SABERTOOTH_BAUD_RATE); // communicating over Serial1
-  ST.autobaud(); // sabertooth algorithm to determine its baudrate
+  Serial.begin(DEFAULT_HOST_SERIAL_BAUD_RATE);
 
-  // driver = MotorDriver(DEFAULT_HOST_SERIAL_BAUD_RATE, std::array<MotorDriver::MotorDriverConfig, MAX_MOTOR_ID + 1>());
+  driver = MotorDriver(DEFAULT_HOST_SERIAL_BAUD_RATE);
 
   if (driver.initMotorDriver() == false)
   {
-    Serial.println("init failed");
+    DEBUG_PRINTLN("Initialization Failed");
   }
 }
 
