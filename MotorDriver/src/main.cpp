94deea6f32e038555166a0ed34a5a12be397c18e
Aastha <aasthagrover05@gmail.com>
2024-01-27 12:06:04 -0800

    finished cytron test

--- MotorDriver/src/main.cpp
+++ MotorDriver/src/main.cpp
@@ -39,7 +39,35 @@
   
 // }
 
+// #define MotorDirection 8
+// #define MotorSpeed 9
 
+// int SpeedVal = 0;
+
+// void setup() {
+//   //Declaration for the pins used, both should be outputs.
+//   pinMode(MotorDirection, OUTPUT);
+//   pinMode(MotorSpeed, OUTPUT);
+// }
+
+// void loop() {
+  
+//   //Ramps up the speed in the clockwise direction.
+//   digitalWrite(MotorDirection, LOW);                  //Loop increases the speed slowly until it reaches max speed.
+//   for(SpeedVal = 0; SpeedVal < 255; SpeedVal++){
+//       analogWrite(MotorSpeed,SpeedVal);
+//       delay(40);
+//   }
+//   analogWrite(MotorSpeed,0);
+//   //Ramps up the speed in the counter clockwise direction.  
+//   digitalWrite(MotorDirection, HIGH);                //Loop increases the speed slowly until it reaches max speed.
+//   for(SpeedVal = 0; SpeedVal < 255; SpeedVal++){
+//       analogWrite(MotorSpeed,SpeedVal);
+//       delay(40);
+//   } 
+//   analogWrite(MotorSpeed,0);
+  
+// }
 CytronMD motor(PWM_DIR, 9, 8);  // PWM = Pin 9, DIR = Pin 8.
 
 
@@ -58,7 +86,7 @@ void loop() {
   delay(1000);
 
   motor.setSpeed(0);    // Stop.
-  delay(1000);
+  delay(500);
 
   motor.setSpeed(-128);  // Run backward at 50% speed.
   delay(1000);
@@ -67,21 +95,21 @@ void loop() {
   delay(1000);
 
   motor.setSpeed(0);    // Stop.
-  delay(1000);
+  delay(500);
 }
 
-//Sabertooth ST(128);
+// //Sabertooth ST(128);
 
-// MotorDriver driver;
+// // MotorDriver driver;
 
-// void setup() {
-//   SabertoothTXPinSerial.begin(9600); // 9600 is the default baud rate for Sabertooth packet serial.
-//   Serial.begin(112500);//For printing
-//   ST.autobaud();
+// // void setup() {
+// //   SabertoothTXPinSerial.begin(9600); // 9600 is the default baud rate for Sabertooth packet serial.
+// //   Serial.begin(112500);//For printing
+// //   ST.autobaud();
 
-//   driver.init_motor_driver();
-// }
+// //   driver.init_motor_driver();
+// // }
 
-// void loop() {
-//   driver.update();
-// }
+// // void loop() {
+// //   driver.update();
+// // }
