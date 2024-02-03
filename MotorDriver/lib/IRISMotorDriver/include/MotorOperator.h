02d2a6169e9ecfcc805afce794df5cea7aeb37e8
rpatkar2 <rpatkar2@illinois.edu>
2024-02-03 12:16:05 -0800

    made motor operator class methods

--- MotorDriver/lib/IRISMotorDriver/include/MotorOperator.h
+++ MotorDriver/lib/IRISMotorDriver/include/MotorOperator.h
@@ -4,13 +4,20 @@
 class MotorOperator
 {
     public:
+        // constructor
+        MotorOperator(bool inverted, bool enabled);
+        MotorOperator();
+        MotorOperator(const MotorOperator& other);
+        MotorOperator& operator=(const MotorOperator& other);
+
         // general
         void setInverted(bool inverted);
         void setEnabled(bool enabled);
+        bool getInverted();
         bool getEnabled();
 
-        // virtual
-        virtual void init();
+        // (pure) virtual
+        virtual bool init() = 0;
 
     private:
         // general
