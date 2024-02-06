c41ba91a9673e07065000371e81259c4a85a7342
Albert Qin <dyrelicgd@gmail.com>
2024-02-06 09:48:09 -0800

    Wrote __init__.py

--- motor_driver/motor_driver/__init__.py
+++ motor_driver/motor_driver/__init__.py
@@ -1,1 +1,18 @@
+import threading
+
 from .motor_driver import MotorDriver
+from .motor_driver import MotorConfig
+from .motor_driver import Serial_Reader
+
+MotorID = 3         # 1-3
+numMotors = 2       # 1-2
+SerialBaud = 9600   # Set at 9600 in C++ code but is 115200 in py ~?
+SerialLine = 2      # 0-2
+Address = 0x000000  # TODO Address of python controller?
+
+config = MotorConfig(SerialLine, numMotors, Address)
+Driver = MotorDriver()
+
+Driver.resetDevice
+Driver.initMotorDriver() # TODO
+Driver.setMotorConfig() # TODO
