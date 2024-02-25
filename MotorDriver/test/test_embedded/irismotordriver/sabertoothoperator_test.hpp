e001e7722f8daab6bfd7a18d0a6e40ba55553df9
ttchalakov <teodor.tch@gmail.com>
2024-02-25 12:09:43 -0600

    Fixed enabled and config update bug

--- MotorDriver/test/test_embedded/irismotordriver/sabertoothoperator_test.hpp
+++ MotorDriver/test/test_embedded/irismotordriver/sabertoothoperator_test.hpp
@@ -13,18 +13,20 @@ void require_disabled_for_update_test(void)
     Sabertooth_Config_Data update_address;
     update_address.set_address(130);
     bool successful = default_operator.applyConfigUpdate(update_address);
-    TEST_ASSERT_FALSE(successful);
+    // Allowed to apply update on disabled config
+    TEST_ASSERT_TRUE(successful);
 
     // Update to enabled
     Sabertooth_Config_Data update_enabled;
     update_enabled.set_enabled(true);
     successful = default_operator.applyConfigUpdate(update_enabled);
+    // Only allowed to apply enabled update regardless of previous enable status
     TEST_ASSERT_TRUE(successful);
     TEST_ASSERT_TRUE(default_operator.getEnabled());
 
-    // Apply update
+    // Fail to apply update to enabled config
     successful = default_operator.applyConfigUpdate(update_address);
-    TEST_ASSERT_TRUE(successful);
+    TEST_ASSERT_FALSE(successful);
 
     // Sanity check
     update_enabled.set_enabled(false);
@@ -33,7 +35,7 @@ void require_disabled_for_update_test(void)
     TEST_ASSERT_FALSE(default_operator.getEnabled());
 
     successful = default_operator.applyConfigUpdate(update_address);
-    TEST_ASSERT_FALSE(successful);
+    TEST_ASSERT_TRUE(successful);
 }
 
 void update_key_not_set_test(void)
