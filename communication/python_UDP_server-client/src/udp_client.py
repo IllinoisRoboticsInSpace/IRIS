67d9e82839e55bafa34ac8de653141e9fbd99d67
Rushil Shah <rushils4@illinois.edu>
2024-11-18 04:07:28 +0000

    Began Server, Added Pipeline Description

--- communication/python_UDP_server-client/src/udp_client.py
+++ communication/python_UDP_server-client/src/udp_client.py
@@ -3,7 +3,13 @@
 @author Rushil Shah (email=rushils4@illinois.edu)
 @maintainer Rushil Shah (email=shahrushil520@gmail.com)
 @date November 17th, 2024
-@brief Python-Based UDP Server for Laptop to Communicate Game Controller Data to Jetson Orin Nano
+
+@brief Python-Based UDP Client for Laptop to Communicate 
+Game Controller Data to Jetson Orin Nano
+
+@description Subscribe to gamepad topics to receive filtered data 
+from gamepad_node, encode data w/ Protobuf, send data to server 
+on Jetson Orin Nano
 """
 
 def main(args=None):
--- communication/python_UDP_server-client/src/udp_server.py
+++ communication/python_UDP_server-client/src/udp_server.py
@@ -3,11 +3,50 @@
 @author Rushil Shah (email=rushils4@illinois.edu)
 @maintainer Rushil Shah (email=shahrushil520@gmail.com)
 @date November 17th, 2024
-@brief Python-Based UDP Client for Jetson Orin Nano to Receive Game Controller Data from Laptop
+
+@brief Python-Based UDP Server for Jetson Orin Nano to 
+Receive Game Controller Data from Laptop
+
+@description Receive data from laptop client, decode message w/ 
+Protobuf, publish to gamepad topics for arduino_comms_node.
 """
 
+import rclpy
+from rclpy.node import Node
+import socket
+
+class PythonUDP_Server(Node):
+
+    sock : socket
+
+    def __init__(self):
+        super().__init__("python_udp_server")
+        self.sock = socket.socket(
+            socket.AF_INET, 
+            socket.SOCK_DGRAM
+        )
+        self.sock.bind(('0.0.0.0', 8080))
+        self.get_logger().info("Python UDP Server listening on port 8080")
+
+        while True:
+            # Record the data and what IP address it's from with a buffer size 1024 bytes.
+            data, address = self.sock.recvfrom(1024)
+
+            # Log Data Before Decoding
+            self.get_logger().info(f"Received: {data} from {address}")
+
+            # Decode the Protobuf Message
+
 def main(args=None):
-    pass
+    rclpy.init(args=args)
+    server : PythonUDP_Server = PythonUDP_Server()
+    try:
+        rclpy.spin(server)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
