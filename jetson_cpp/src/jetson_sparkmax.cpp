715c1f4b814454170ef4b3d1d1dac5bd27f5401e
iris-uiuc <iris.uiuc@gmail.com>
2025-02-22 14:20:45 -0600

    jetson_sparkmax.cpp Fix Attempt #1

--- jetson_cpp/src/jetson_sparkmax.cpp
+++ jetson_cpp/src/jetson_sparkmax.cpp
@@ -6,11 +6,16 @@
 #include "SparkMax.hpp"
 #include "constants.hpp"
 
-#define N_INPUTS = 6
+#define N_INPUTS 6
 
-std::string[] keys = ["left_drive", "right_drive", 
-                      "excavator_lin_actr",
-                      "auto_mode", "stop_mode", "speed_mode"];
+constexpr std::array<const char*, 6> KEYS = {
+                    "left_drive", 
+                    "right_drive", 
+                    "excavator_lin_actr",
+                    "auto_mode", 
+                    "stop_mode", 
+                    "speed_mode"
+};
 
 class JetsonSparkMax : public rclcpp::Node {
 
@@ -19,78 +24,115 @@ class JetsonSparkMax : public rclcpp::Node {
             : Node("jetson_sparkmax"),
             m_left_drive("can0", constants::LEFT_DRIVE),
             m_right_drive("can0", constants::RIGHT_DRIVE),
-            m_left_back_coll("can0", constants::LEFT_BACK_COLL),
-            m_right_back_coll("can0", constants::RIGHT_BACK_COLL),
-            m_exc_internal("can0", constants::EXC_INTERNAL),
-            m_exc_thread_rod("can0", constants::EXC_THREAD_ROD),
-            m_exc_pivot_lin("can0", constants::EXC_PIVOT_LIN) {
+            m_excavator_lin_actr("can0", constants::LEFT_BACK_COLL),
+            lock_(std::make_shared<std::mutex>()) {
+
             logger_ = this->get_logger();
 
             // Initialize gamepad subscriptions
-            for (const auto& mapping : get_gamepad_mapping()) {
-                const std::string& key = mapping.first;
-                int value = mapping.second;
-                rclcpp::MessageType msg_type = (key == "left_drive" || key == "right_drive") ?
-                    std::make_shared<std_msgs::msg::Float32>() : std::make_shared<std_msgs::msg::Bool>();
-                auto callback = std::bind(&JetsonCommsNode::motor_driver_callback, this, _1, key);
-                gamepad_subscribers[value] = create_subscription<decltype(msg_type)>(
-                    f"/gamepad/{key}", 10, callback);
-                RCLCPP_INFO(logger_, "Created node %s", this->get_name());
+            for (const auto& key : KEYS) {
+                
+                if (key == "left_drive" || key == "right_drive") {
+                    auto subscriber = this->create_subscription<std_msgs::msg::Float32>(
+                        std::string("/gamepad/") + key, 
+                        10, 
+                        std::bind(
+                            &JetsonSparkMax::motor_driver_callback_float,
+                            this,
+                            std::placeholder::_1,
+                            key
+                        )
+                    );
+                    gamepad_subscribers_.push_back(subscriber);
+                } else {
+                    auto subscriber = this->create_subscription<std_msgs::msg::Bool>(
+                        std::string("/gamepad/") + key,
+                        10,
+                        std::bind(
+                            &JetsonSparkMax::motor_driver_callback_bool,
+                            this, 
+                            std::placeholders::_1, 
+                            key
+                        )
+                    );
+                    gamepad_subscribers_.push_back(subscriber);
+                }
+                RCLCPP_INFO(logger_, "Created Subscription %s", this->get_name());
             }
         }
     private:
+
         std::shared_ptr<std::mutex> lock_;
         std::vector<rclcpp::SubscriptionBase::SharedPtr> gamepad_subscribers_;
-        MotorDriver[2] motor_drivers_;
+        MotorDriver motor_drivers_[2];
         MotorDriver lin_actuator_driver; /* TODO: Overload MotorDriver Functions for Cytron */
         rclcpp::Logger logger_;
         
-        void motor_driver_callback(const std::shared_ptr<void> msg, const std::string& motor) {
+        void motor_driver_callback_float(
+            const std_msgs::msg::Float32::SharedPtr msg, 
+            const std::string& motor) {
+
             std::lock_guard<std::mutex> lock(lock_);
-            if (std::holds_alternative<std_msgs::msg::Float32>(msg)) {
-                const auto& float_msg = std::get<std_msgs::msg::Float32>(msg);
-                RCLCPP_INFO(logger_, "Data received on %s: %f", motor.c_str(), float_msg.data);
-            } else if (std::holds_alternative<std_msgs::msg::Bool>(msg)) {
-                const auto& bool_msg = std::get<std_msgs::msg::Bool>(msg);
-                RCLCPP_INFO(logger_, "Data received on %s: %s", motor.c_str(), bool_msg.data ? "true" : "false");
-            }
+
+            RCLCPP_INFO(logger_, "Data received on %s: %f", motor.c_str(), msg.data);
+
             if (motor == "left_drive") {
                 m_left_drive.Heartbeat();
             } else if (motor == "right_drive") {
                 m_right_drive.Heartbeat();
-            } else if (motor == "left_back_cltn_mtr") {
-                m_left_back_coll.Heartbeat();
-            } else if (motor == "right_back_cltn_mtr") {
-                m_right_back_coll.Heartbeat();
-            } else if (motor == "exc_intrnl_mtr") {
-                m_exc_internal.Heartbeat();
-            } else if (motor == "exc_thrd_rod_actr") {
-                m_exc_thread_rod.Heartbeat();
-            } else if (motor == "exc_pvt_lin_actr") {
-                m_exc_pivot_lin.Heartbeat();
+            } else if (motor == "excavator_lin_actr") {
+                //m_excavator_lin_actr.Heartbeat();
             } else if (motor == "auto_mode") {
                 switch_to_auto_mode();
             } else if (motor == "stop_mode") {
                 stop_motors();
+            } else if (motor == "speed_mode") {
+                switch_to_speed_mode();
             }
         }
+
+        void motor_driver_callback_bool(
+            const std_msgs::msg::Bool::SharedPtr msg, 
+            const std::string& motor) {
+
+            std::lock_guard<std::mutex> lock(lock_);
+
+            RCLCPP_INFO(logger_, "Data received on %s: %f", motor.c_str(), msg.data ? "true" : "false");
+
+            if (motor == "left_drive") {
+                m_left_drive.Heartbeat();
+            } else if (motor == "right_drive") {
+                m_right_drive.Heartbeat();
+            } else if (motor == "excavator_lin_actr") {
+                m_excavator_lin_actr.Heartbeat();
+            } else if (motor == "auto_mode") {
+                switch_to_auto_mode();
+            } else if (motor == "stop_mode") {
+                stop_motors();
+            } else if (motor == "speed_mode") {
+                switch_to_speed_mode();
+            }
+        }
+
         void switch_to_auto_mode() {
             // Not implemented in original
         }
+
+        void switch_to_speed_mode() {
+            // Not implemented
+        }
+
         void stop_motors() {
             m_left_drive.SetDutyCycle(0);
             m_right_drive.SetDutyCycle(0);
-            m_left_back_coll.SetDutyCycle(0);
-            m_right_back_coll.SetDutyCycle(0);
-            m_exc_internal.SetDutyCycle(0);
-            m_exc_pivot_lin.SetDutyCycle(0);
-            m_exc_thread_rod.SetDutyCycle(0);
+            m_excavator_lin_actr.SetDutyCycle(0);
         }
 };
+
 // Entry function
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<JetsonCommsNode>());
+    rclcpp::spin(std::make_shared<JetsonSparkMax>());
     rclcpp::shutdown();
     return 0;
 }
