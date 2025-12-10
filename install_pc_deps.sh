40a08e330e588d9840b45e41411d4583160298ea
iris-uiuc <iris.uiuc@gmail.com>
2025-12-09 18:59:20 -0600

    Updated install script

--- install_pc_deps.sh
+++ install_pc_deps.sh
@@ -4,26 +4,32 @@
 #   ONLY RUN THIS SCRIPT ONCE
 #
 
-echo "Setting Up IRIS Jazzy Development Environment Dependencies"
+echo "Setting Up IRIS Humble Development Environment Dependencies"
 
 export DEBIAN_FRONTEND=noninteractive
 
-# Install ROS 2 Jazzy
+# Install ROS 2 Humble
 # Initialization
 sudo apt update -y && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-sudo apt update -y && sudo apt install -y curl gnupg lsb-release
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y universe
+
+sudo apt update -y && sudo apt install -y curl
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
 sudo apt update -y
 sudo apt upgrade -y
 
-# Install Jazzy
-sudo apt install -y ros-jazzy-desktop
-echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
-source /opt/ros/jazzy/setup.bash
+# Install Humble
+sudo apt install -y ros-humble-desktop
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source /opt/ros/humble/setup.bash
 
 # Utilities
 sudo apt-get install -y python3-colcon-common-extensions python3-rosdep
