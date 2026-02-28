5f668b93919c265e4e1857fe5ca1677c92be1fe9
DivAgarwal1 <divca06@gmail.com>
2026-02-28 12:49:42 -0600

    setup import for linear actuator

--- depositor/depositor/depositor_node.py
+++ depositor/depositor/depositor_node.py
@@ -1,12 +1,16 @@
-import rclpy, copy
+import rclpy
+import copy
 from dataclasses import dataclass
 from rclpy.node import Node
 from std_msgs.msg import Bool
 import time
 
+from linear_actuator import LinearActuator
+
 hold_time = 5
 motor_speed = 0.5
 motor_rest = 0
+
 
 class Depositer(Node):
     def __init__(self):
@@ -14,21 +18,26 @@ class Depositer(Node):
 
         self.linear_actuator = LinearActuator(123, 456, 789)
 
-        self.dep_routine_publisher = self.create_publisher(Bool, 'dep_routine', 10)
+        self.dep_routine_publisher = self.create_publisher(
+            Bool, 'dep_routine', 10)
 
         self.timer = self.create_timer(0.1, self.timer_response)
-        self.raise_dumper_teleop_subscriber = self.create_subcription(Bool, 'raise_dumper_teleop', self.raise_dumper_response, 10)
-        self.lower_dumper_teleop_subscriber = self.create_subcription(Bool, 'lower_dumper_teleop', self.lower_dumper_response, 10)
-        self.dep_routine_subscription = self.create_subscription(Bool, 'dep_routine', self.dep_routine_response, 10)
-        self.dumper_max_limit_switch_subscription = self.create_subscription(Bool, 'dumper_max_limit_switch', self.dumper_max_limit_switch_response, 10)
-        self.dumper_min_limit_switch_subscription = self.create_subscription(Bool, 'dumper_min_limit_switch', self.dumper_min_limit_switch_response, 10)
-        
+        self.raise_dumper_teleop_subscriber = self.create_subcription(
+            Bool, 'raise_dumper_teleop', self.raise_dumper_response, 10)
+        self.lower_dumper_teleop_subscriber = self.create_subcription(
+            Bool, 'lower_dumper_teleop', self.lower_dumper_response, 10)
+        self.dep_routine_subscription = self.create_subscription(
+            Bool, 'dep_routine', self.dep_routine_response, 10)
+        self.dumper_max_limit_switch_subscription = self.create_subscription(
+            Bool, 'dumper_max_limit_switch', self.dumper_max_limit_switch_response, 10)
+        self.dumper_min_limit_switch_subscription = self.create_subscription(
+            Bool, 'dumper_min_limit_switch', self.dumper_min_limit_switch_response, 10)
+
         self.dep_routine_val = False
         self.max_limit = False
         self.min_limit = False
         self.should_run = False
         self.direction = False
-
 
     def dumper_max_limit_switch_response(self, message: Bool):
         self.max_limit = message.data
@@ -38,7 +47,6 @@ class Depositer(Node):
 
     def timer_response(self):
         if self.should_run and not self.max_limit and not self.min_limit:
-            
 
     def dep_routine_response(self, message: Bool):
         self.dep_routine_val = message.data
@@ -54,7 +62,6 @@ class Depositer(Node):
 
             self.dep_routine_publisher.publish(Bool(data=False))
 
-
     def raise_dumper_response(self, message: Bool):
         if not self.dep_routine_val and message.data:
             self.should_run = True
@@ -67,16 +74,6 @@ class Depositer(Node):
         if not self.dep_routine_val and message.data:
             self.should_run = True
             self.direction = False
-        
+
         else:
             self.should_run = False
-            
-
-
-
-
-
-                
-        
-
-
